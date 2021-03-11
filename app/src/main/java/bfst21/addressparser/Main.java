47c30e653912e4840e69ee8e46aa77523c0391f2
Thea <tkjeldsmark@gmail.com>
2021-03-11 18:21:03 +0100

    Delete files and organize folders

--- app/src/main/java/bfst21/addressparser/Main.java
+++
@@ -1,8 +0,0 @@
-package bfst21.addressparser;
-
-public class Main {
-    public static void main(String[] args) {
-        GUI.launch(GUI.class, args);
-        
-    }
-}
--- app/src/main/java/bfst21/addressparser/badInputException.java
+++
@@ -1,7 +0,0 @@
-package bfst21.addressparser;
-
-public class badInputException extends Exception{
-    public badInputException(String message){
-        super(message);
-    }
-}
