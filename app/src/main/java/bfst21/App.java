d372cece603fb604e0b724b5d33117c15c2f693a
tkje <tkje@itu.dk>
2021-03-11 18:29:37 +0100

    Organize files

--- app/src/main/java/bfst21/App.java
+++ app/src/main/java/bfst21/App.java
@@ -5,7 +5,7 @@ import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         /*var filename = getParameters().getRaw().get(0);
         var model = new Model(filename);
         new View(model, primaryStage);*/
